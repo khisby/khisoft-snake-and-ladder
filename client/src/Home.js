3095955ca8e38ff4bcb129978abb7ffd13ccae4c
Khisby <khisby@gmail.com>
2020-06-05 23:06:28 +0700

    add home page

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,11 +1,14 @@
 import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
+import Button from 'react-bootstrap/Button';
+
 const Home = () => {
     const [teks, setText] = useState(123123123)
     return (<div>
         Hallo Home
         <Link to="/create">Buat Room Baru</Link>
         <Link to={ `/${teks}` }>Join</Link>
+        
     </div>);
 }
  
